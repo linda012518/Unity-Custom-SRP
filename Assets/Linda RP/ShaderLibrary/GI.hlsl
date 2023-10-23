d4744aa38d11846dbfb1e0fcae9d0cfa2b64923d
LAPTOP-MFP98GMN\GD-NPC-880 <895613747@qq.com>
2023-10-23 14:00:30 +0800

    采样lightmap，UV没有bug

--- Assets/Linda RP/ShaderLibrary/GI.hlsl
+++ Assets/Linda RP/ShaderLibrary/GI.hlsl
@@ -3,6 +3,9 @@
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/EntityLighting.hlsl"
 
+TEXTURE2D(unity_Lightmap);
+SAMPLER(samplerunity_Lightmap);
+
 #if defined(LIGHTMAP_ON)
 	#define GI_ATTRIBUTE_DATA float2 lightMapUV : TEXCOORD1;
 	#define GI_VARYINGS_DATA float2 lightMapUV : VAR_LIGHT_MAP_UV;
@@ -22,10 +25,28 @@ struct GI
 	float3 diffuse;
 };
 
+float3 SampleLightMap(float2 lightMapUV)
+{
+#if defined(LIGHTMAP_ON)
+	//float4 transform 是UV的缩放和平移，在11行代码里做了，这里不管
+	//bool encodedLightmap 是否有压缩
+	//real4 decodeInstructions 解码指令
+	return SampleSingleLightmap(TEXTURE2D_ARGS(unity_Lightmap, samplerunity_Lightmap), lightMapUV, float4(1.0, 1.0, 0.0, 0.0),
+			#if defined(UNITY_LIGHTMAP_FULL_HDR)
+				false,
+			#else
+				true,
+			#endif
+			float4(LIGHTMAP_HDR_MULTIPLIER, LIGHTMAP_HDR_EXPONENT, 0.0, 0.0));
+#else
+	return 0.0;
+#endif
+}
+
 GI GetGI(float2 lightMapUV)
 {
 	GI gi;
-	gi.diffuse = float3(lightMapUV, 0.0);
+	gi.diffuse = SampleLightMap(lightMapUV);
 	return gi;
 }
 
