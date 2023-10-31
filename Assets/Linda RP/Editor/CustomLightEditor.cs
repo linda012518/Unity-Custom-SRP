690b41b2f6338125b67ef581f71c81507876599f
LAPTOP-MFP98GMN\GD-NPC-880 <895613747@qq.com>
2023-10-31 19:57:55 +0800

    编辑器配置聚光灯内角

---
+++ Assets/Linda RP/Editor/CustomLightEditor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditorForRenderPipeline(typeof(Light), typeof(LindaRenderPipelineAsset))]
+public class CustomLightEditor : LightEditor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		if (!settings.lightType.hasMultipleDifferentValues && (LightType)settings.lightType.enumValueIndex == LightType.Spot)
+		{
+			settings.DrawInnerAndOuterSpotAngle();
+			settings.ApplyModifiedProperties();
+		}
+	}
+}
