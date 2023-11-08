1132f620805271732894a2364e29502683eb656b
LAPTOP-MFP98GMN\GD-NPC-880 <895613747@qq.com>
2023-11-08 19:19:23 +0800

    ACES 在  ACEScg 色彩空间

--- Assets/Linda RP/Shaders/PostFXStackPasses.hlsl
+++ Assets/Linda RP/Shaders/PostFXStackPasses.hlsl
@@ -207,6 +207,10 @@ float4 _SplitToningShadows, _SplitToningHighlights;
 float4 _ChannelMixerRed, _ChannelMixerGreen, _ChannelMixerBlue;
 float4 _SMHShadows, _SMHMidtones, _SMHHighlights, _SMHRange;
 
+float Luminance (float3 color, bool useACES) {
+	return useACES ? AcesLuminance(color) : Luminance(color);
+}
+
 float3 ColorGradePostExposure (float3 color) {
 	//曝光
 	return color * _ColorAdjustments.x;
@@ -218,11 +222,11 @@ float3 ColorGradeWhiteBalance (float3 color) {
 	return LMSToLinear(color);
 }
 
-float3 ColorGradingContrast (float3 color) {
+float3 ColorGradingContrast (float3 color, bool useACES) {
 	//对比度
-	color = LinearToLogC(color);
+	color = useACES ? ACES_to_ACEScc(unity_to_ACES(color)) : LinearToLogC(color);
 	color = (color - ACEScc_MIDGRAY) * _ColorAdjustments.y + ACEScc_MIDGRAY;
-	return LogCToLinear(color);
+	return useACES ? ACES_to_ACEScg(ACEScc_to_ACES(color)) : LogCToLinear(color);
 }
 
 float3 ColorGradeColorFilter (float3 color) {
@@ -238,16 +242,16 @@ float3 ColorGradingHueShift (float3 color) {
 	return HsvToRgb(color);
 }
 
-float3 ColorGradingSaturation (float3 color) {
+float3 ColorGradingSaturation (float3 color, bool useACES) {
 	//饱和度
-	float luminance = Luminance(color);
+	float luminance = Luminance(color, useACES);
 	return (color - luminance) * _ColorAdjustments.w + luminance;
 }
 
-float3 ColorGradeSplitToning (float3 color) {
+float3 ColorGradeSplitToning (float3 color, bool useACES) {
 	//色调分离
 	color = PositivePow(color, 1.0 / 2.2);
-	float t = saturate(Luminance(saturate(color)) + _SplitToningShadows.w);
+	float t = saturate(Luminance(saturate(color), useACES) + _SplitToningShadows.w);
 	float3 shadows = lerp(0.5, _SplitToningShadows.rgb, 1.0 - t);
 	float3 highlights = lerp(0.5, _SplitToningHighlights.rgb, t);
 	color = SoftLight(color, shadows);
@@ -263,9 +267,9 @@ float3 ColorGradingChannelMixer (float3 color) {
 	);
 }
 
-float3 ColorGradingShadowsMidtonesHighlights (float3 color) {
+float3 ColorGradingShadowsMidtonesHighlights (float3 color, bool useACES) {
 	//调阴影、中间调、高光
-	float luminance = Luminance(color);
+	float luminance = Luminance(color, useACES);
 	float shadowsWeight = 1.0 - smoothstep(_SMHRange.x, _SMHRange.y, luminance);
 	float highlightsWeight = smoothstep(_SMHRange.z, _SMHRange.w, luminance);
 	float midtonesWeight = 1.0 - shadowsWeight - highlightsWeight;
@@ -275,21 +279,21 @@ float3 ColorGradingShadowsMidtonesHighlights (float3 color) {
 		color * _SMHHighlights.rgb * highlightsWeight;
 }
 
-float3 ColorGrade(float3 color)
+float3 ColorGrade(float3 color, bool useACES = false)
 {
 	color = min(color, 60);
 	color = ColorGradePostExposure(color);
 	color = ColorGradeWhiteBalance(color);
-	color = ColorGradingContrast(color);
+	color = ColorGradingContrast(color, useACES);
 	color = ColorGradeColorFilter(color);
 	color = max(color, 0.0);
-	color = ColorGradeSplitToning(color);
+	color = ColorGradeSplitToning(color, useACES);
 	color = ColorGradingChannelMixer(color);
 	color = max(color, 0.0);
-	color = ColorGradingShadowsMidtonesHighlights(color);
+	color = ColorGradingShadowsMidtonesHighlights(color, useACES);
 	color = ColorGradingHueShift(color);
-	color = ColorGradingSaturation(color);
-	return max(color, 0.0);
+	color = ColorGradingSaturation(color, useACES);
+	return  max(useACES ? ACEScg_to_ACES(color) : color, 0.0);
 }
 
 float4 ColorGradingNonePassFragment (Varyings input) : SV_TARGET {
@@ -315,8 +319,8 @@ float4 ToneMappingNeutralPassFragment (Varyings input) : SV_TARGET {
 
 float4 ToneMappingACESPassFragment (Varyings input) : SV_TARGET {
 	float4 color = GetSource(input.screenUV);
-	color.rgb = ColorGrade(color.rgb);
-	color.rgb = AcesTonemap(unity_to_ACES(color.rgb));
+	color.rgb = ColorGrade(color.rgb, true);
+	color.rgb = AcesTonemap(color.rgb);
 	return color;
 }
 
