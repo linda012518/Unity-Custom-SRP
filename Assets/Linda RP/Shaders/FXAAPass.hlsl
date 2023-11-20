f21d8a56065681f53563fc02874b77d43df0dfef
LAPTOP-MFP98GMN\GD-NPC-880 <895613747@qq.com>
2023-11-20 16:24:20 +0800

    只在一边方向混合

--- Assets/Linda RP/Shaders/FXAAPass.hlsl
+++ Assets/Linda RP/Shaders/FXAAPass.hlsl
@@ -122,24 +122,24 @@ float GetEdgeBlendFactor (LumaNeighborhood luma, FXAAEdge edge, float2 uv) {
 	float gradientThreshold = 0.25 * edge.lumaGradient;
 
 	float2 uvP = edgeUV + uvStep;
-	float lumaGradientP = abs(GetLuma(uvP) - edgeLuma);
-	bool atEndP = lumaGradientP >= gradientThreshold;
+	float lumaDeltaP = GetLuma(uvP) - edgeLuma;
+	bool atEndP = abs(lumaDeltaP) >= gradientThreshold;
 
 	int i;
 	for (i = 0; i < 99 && !atEndP; i++) {
 		uvP += uvStep;
-		lumaGradientP = abs(GetLuma(uvP) - edgeLuma);
-		atEndP = lumaGradientP >= gradientThreshold;
+		lumaDeltaP = GetLuma(uvP) - edgeLuma;
+		atEndP = abs(lumaDeltaP) >= gradientThreshold;
 	}
 
 	float2 uvN = edgeUV - uvStep;
-	float lumaGradientN = abs(GetLuma(uvN) - edgeLuma);
-	bool atEndN = lumaGradientN >= gradientThreshold;
+	float lumaDeltaN = GetLuma(uvN) - edgeLuma;
+	bool atEndN = abs(lumaDeltaN) >= gradientThreshold;
 
 	for (i = 0; i < 99 && !atEndN; i++) {
 		uvN -= uvStep;
-		lumaGradientN = abs(GetLuma(uvN) - edgeLuma);
-		atEndN = lumaGradientN >= gradientThreshold;
+		lumaDeltaN = GetLuma(uvN) - edgeLuma;
+		atEndN = abs(lumaDeltaN) >= gradientThreshold;
 	}
 
 	float distanceToEndP, distanceToEndN;
@@ -154,14 +154,22 @@ float GetEdgeBlendFactor (LumaNeighborhood luma, FXAAEdge edge, float2 uv) {
 	}
 
 	float distanceToNearestEnd;
+	bool deltaSign;
 	if (distanceToEndP <= distanceToEndN) {
 		distanceToNearestEnd = distanceToEndP;
+		deltaSign = lumaDeltaP >= 0;
 	}
 	else {
 		distanceToNearestEnd = distanceToEndN;
+		deltaSign = lumaDeltaN >= 0;
 	}
 
-	return 10.0 * distanceToNearestEnd;
+	if (deltaSign == (luma.m - edgeLuma >= 0)) {
+		return 0.0;
+	}
+	else {
+		return 10.0 * distanceToNearestEnd;
+	}
 }
 
 float4 FXAAPassFragment (Varyings input) : SV_TARGET {
